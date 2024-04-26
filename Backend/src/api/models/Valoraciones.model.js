fa343cd2f8a3d54870a65d6343a7f643ef8cfbf4
Your Name <you@example.com>
2024-04-26 11:30:48 +0200

    Server

---
+++ Backend/src/api/models/Valoraciones.model.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt"); // para encryptar informacion
+const validator = require("validator"); /// n os sirve para validad info
+const mongoose = require("mongoose");
+
+const ValoracionSchema = new mongoose.Schema(
+  {
+    valoracion: { type: mongoose.Schema.Types.ObjectId, ref: "Valoraciones" },
+    fechaCreacion: {
+      type: Date,
+      default: Date.now,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    rating: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+      require: true,
+    },
+    review: {
+      type: String,
+      require: false,
+    },
+    activityId: { type: mongoose.Schema.Types.ObjectId, ref: "Activity" },
+    receptorId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = ValoracionSchema;
