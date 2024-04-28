b5fea6eeb459bf1057cb6ba38931dbdc16f42c8d
sarodri <sandra.ropero1987@gmail.com>
2024-04-28 07:57:48 +0200

    codigo message

--- Backend/src/api/controllers/Message.controllers.js
+++ Backend/src/api/controllers/Message.controllers.js
@@ -5,22 +5,18 @@ const Message = require("../models/Message.model");
 const Wall = require("../models/Wall.model");
 const User = require("../models/User.model");
 
+
+
+//! -----------------------------------------------------------------------------
+//? ----------------------------CREATE MESSAGE-----------------------------------
+//! -----------------------------------------------------------------------------
+
 const createMessage = async (req, res, next) => {
     try {
       const { type, content } = req.body;
-      const { idRecipient } = req.params; // -----> id de a quien quiero hacer el comentario
-      /**
-       * idRecipient puede ser el id de : movie, character, user
-       */
+      const { idRecipient } = req.params; // id de a quien quiero hacer el comentario
   
       const findUser = await User.findById(idRecipient);
-      // const findMonitor = await User.findById(idRecipient);
-    
-  
-      /**
-       * cuando no lo encuentre devuelve un null y el que encuentre va a devolver el objeto encontrado
-       *
-       */
   
       if (findUser) {
         const newMessage = new Message(req.body);
@@ -29,8 +25,8 @@ const createMessage = async (req, res, next) => {
         if (type == "private") {
           try {
             const chatExistOne = await Chat.findOne({
-              userOne: req.user._id,
-              userTwo: findUser._id,
+              userOne: req.user._id, //usuario logado
+              userTwo: findUser._id, //usuario que recibe
             });
             const chatExistTwo = await Chat.findOne({
               userOne: findUser._id,
@@ -49,12 +45,12 @@ const createMessage = async (req, res, next) => {
               //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
               // ---------------------------- CHAT EXISTE: TENEMOS QUE ACTUALIZARLO -------------------------------------
               //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-              ///&/ existe un chat y entonces lo actualizamos conm el nuevo mensaje
+              ///Si existe un chat, entonces lo actualizamos con el nuevo mensaje
   
               if (chatExistOne) {
                 try {
                   await chatExistOne.updateOne({
-                    /** podemos hacer un push */
+                    // podemos hacer un push 
                     $push: { messages: newMessage._id },
                   });
                   try {
@@ -89,7 +85,6 @@ const createMessage = async (req, res, next) => {
                   await chatExistTwo.updateOne({
                     $push: { messages: newMessage._id },
                   });
-  
                   try {
                     await User.findByIdAndUpdate(req.user._id, {
                       $push: {
@@ -130,17 +125,15 @@ const createMessage = async (req, res, next) => {
               //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
               // ---------------------------- CREAR CHAT PORQUE NO EXISTE NINGUNO ---------------------------------------
               //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-              /// crear un chat con el comentario que hemos creado
+              /// crear un chat con el mensaje que hemos creado
   
               const newChat = new Chat({
                 userOne: req.user._id,
                 userTwo: findUser._id,
                 messages: [savedMessage._id],
               });
-  
               try {
                 await newChat.save();
-  
                 try {
                   await User.findByIdAndUpdate(req.user._id, {
                     $push: {
@@ -148,14 +141,12 @@ const createMessage = async (req, res, next) => {
                       chats: newChat._id,
                     },
                   });
-  
                   try {
                     await User.findByIdAndUpdate(idRecipient, {
                       $push: {
                         chats: newChat._id,
                       },
                     });
-  
                     return res.status(200).json({
                       chat: await Chat.findById(newChat._id).populate(
                         "messages  userOne  userTwo"
@@ -185,7 +176,6 @@ const createMessage = async (req, res, next) => {
                   return res.status(404).json({
                     error:
                       "no se ha creado el chat pero no se ha borrado el comentario",
-  
                     idMensageNoBorrado: savedMessage._id,
                   });
                 }
@@ -201,14 +191,12 @@ const createMessage = async (req, res, next) => {
                 postedMessages: newMessage._id,
               },
             });
-  
             try {
               await User.findByIdAndUpdate(idRecipient, {
                 $push: {
                   commentsPublicByOther: newMessage._id,
                 },
               });
-  
               return res.status(200).json({
                 userOwner: await User.findById(req.user._id).populate([
                   {
@@ -243,11 +231,15 @@ const createMessage = async (req, res, next) => {
     }
   };
 
+//! -----------------------------------------------------------------------------
+//? ----------------------------DELETE MESSAGE POR USUARIO-----------------------
+//! -----------------------------------------------------------------------------
+
   const deleteMessagesByUser = async (req, res, next) => {
     try {
       const { chatId, userId } = req.params;
   
-      // Encontrar el chat por su ID
+      // Encontrar el chat por su iD
       const chat = await Chat.findById(chatId);
       if (!chat) {
         return res.status(404).json({ error: "Chat no encontrado" });
@@ -271,11 +263,15 @@ const createMessage = async (req, res, next) => {
     }
   };
 
+//! -----------------------------------------------------------------------------
+//? ----------------------------GET MESSAGES BY USER----------------------------
+//! -----------------------------------------------------------------------------
+
   const getById = async (req, res, next) => {
     try {
       const { id } = req.params;
   
-      // Buscamos al usuario por su ID y nos traemos los dos tipos de comentarios que hace
+      // Buscamos al usuario por su iD y nos traemos los dos tipos de comentarios que hace
       const user = await User.findById(id, 'commentsPublicByOther postedMessages')
         .populate('commentsPublicByOther')
         .populate('postedMessages');
@@ -297,209 +293,84 @@ const createMessage = async (req, res, next) => {
   };
   
 
-//   const deleteCharacter = async (req, res, next) => {
-//     try {
-//       const { id } = req.params;
-//       const character = await Character.findByIdAndDelete(id);
-//       if (character) {
-//         // lo buscamos para vr si sigue existiendo o no
-//         const finByIdCharacter = await Character.findById(id);
-  
-//         try {
-//           const test = await Movie.updateMany(
-//             { characters: id },
-//             { $pull: { characters: id } }
-//           );
-//           console.log(test);
-  
-//           try {
-//             await User.updateMany(
-//               { charactersFav: id },
-//               { $pull: { charactersFav: id } }
-//             );
-  
-//             return res.status(finByIdCharacter ? 404 : 200).json({
-//               deleteTest: finByIdCharacter ? false : true,
-//             });
-//           } catch (error) {
-//             return res.status(404).json({
-//               error: "error catch update User",
-//               message: error.message,
-//             });
-//           }
-//         } catch (error) {
-//           return res.status(404).json({
-//             error: "error catch update Movie",
-//             message: error.message,
-//           });
-//         }
-//       }
-//     } catch (error) {
-//       return res.status(404).json(error.message);
-//     }
-//   };
-
- 
- 
-//  const update = async (req, res, next) => {
-//     let catchImg = req.file?.path;
-//     try {
-//       await Character.syncIndexes();
-//       const { id } = req.params;
-//       const characterById = await Character.findById(id);
-//       if (characterById) {
-//         const oldImg = characterById.image;
-  
-//         const customBody = {
-//           _id: characterById._id,
-//           image: req.file?.path ? catchImg : oldImg,
-//           name: req.body?.name ? req.body?.name : characterById.name,
-//         };
-  
-//         if (req.body?.gender) {
-//           const resultEnum = enumOk(req.body?.gender);
-//           customBody.gender = resultEnum.check
-//             ? req.body?.gender
-//             : characterById.gender;
-//         }
-  
-//         try {
-//           await Character.findByIdAndUpdate(id, customBody);
-//           if (req.file?.path) {
-//             deleteImgCloudinary(oldImg);
-//           }
-  
-//           //** ------------------------------------------------------------------- */
-//           //** VAMOS A TESTEAR EN TIEMPO REAL QUE ESTO SE HAYA HECHO CORRECTAMENTE */
-//           //** ------------------------------------------------------------------- */
-  
-//           // ......> VAMOS A BUSCAR EL ELEMENTO ACTUALIZADO POR ID
-  
-//           const characterByIdUpdate = await Character.findById(id);
-  
-//           // ......> me cojer el req.body y vamos a sacarle las claves para saber que elementos nos ha dicho de actualizar
-//           const elementUpdate = Object.keys(req.body);
-  
-//           /** vamos a hacer un objeto vacion donde meteremos los test */
-  
-//           let test = {};
-  
-//           /** vamos a recorrer las claves del body y vamos a crear un objeto con los test */
-  
-//           elementUpdate.forEach((item) => {
-//             if (req.body[item] === characterByIdUpdate[item]) {
-//               test[item] = true;
-//             } else {
-//               test[item] = false;
-//             }
-//           });
-  
-//           if (catchImg) {
-//             characterByIdUpdate.image === catchImg
-//               ? (test = { ...test, file: true })
-//               : (test = { ...test, file: false });
-//           }
-  
-//           /** vamos a ver que no haya ningun false. Si hay un false lanzamos un 404,
-//            * si no hay ningun false entonces lanzamos un 200 porque todo esta correcte
-//            */
-  
-//           let acc = 0;
-//           for (clave in test) {
-//             test[clave] == false && acc++;
-//           }
-  
-//           if (acc > 0) {
-//             return res.status(404).json({
-//               dataTest: test,
-//               update: false,
-//             });
-//           } else {
-//             return res.status(200).json({
-//               dataTest: test,
-//               update: true,
-//             });
-//           }
-//         } catch (error) {
-//           return res.status(404).json(error.message);
-//         }
-//       } else {
-//         return res.status(404).json("este character no existe");
-//       }
-//     } catch (error) {
-//       return res.status(404).json(error.message);
-//     }
-//   };
+//! -----------------------------------------------------------------------------
+//? ----------------------------TOGGLE LIKE MESSAGE -----------------------------
+//! -----------------------------------------------------------------------------
 
 
+  const toggleLikeMuro = async (req, res, next) => {
+    try {
+    //meto el Id del muro en los params
+      const { wallId } = req.params;
+      // vamos a tener el middleware de auth por lo cual se crea req.user
+      const { _id } = req.user;
+    // si en los likes del user está el iD del muro que buscamos, continúo..
+      if (req.user.murosLikes.includes(wallId)) {
+        try { 
+        //actualizo los likes del muro y del user a disliked
+          await User.findByIdAndUpdate(_id, {
+            $pull: { murosLikes: wallId },
+          });
+          try {
+            await Wall.findByIdAndUpdate(wallId, {
+              $pull: { likes: _id },
+            });
+            //populamos la respuesta
+            return res.status(200).json({
+              action: "disliked",
+              user: await User.findById(_id).populate("murosLikes"),
+              wall: await Wall.findById(wallId).populate("likes"),
+            });
+          } catch (error) {
+            return res.status(404).json({
+              error: "no update wall - likes",
+              message: error.message,
+            });
+          }
+        } catch (error) {
+          return res.status(404).json({
+            error: "no update user-  murosLikes",
+            message: error.message,
+          });
+        }
+      } else {
+      //si el id del muro no esta en el user, tenemos que añadirlo para crewar el like
+        try {
+          await User.findByIdAndUpdate(_id, {
+            $push: { murosLikes: wallId },
+          });
+          try {
+            await Wall.findByIdAndUpdate(wallId, {
+              $push: { likes: _id },
+            });
+          // populamos la respuesta
+            return res.status(200).json({
+              action: "like",
+              user: await User.findById(_id).populate("murosLikes"),
+              wall: await Wall.findById(wallId).populate("likes"),
+            });
+          } catch (error) {
+            return res.status(404).json({
+              error: "no update wall - likes",
+              message: error.message,
+            });
+          }
+        } catch (error) {
+          return res.status(404).json({
+            error: "no update user-  murosLikes",
+            message: error.message,
+          });
+        }
+      }
+    } catch (error) {
+      return res.status(404).json(error.message);
+    }
+  };
+  
 
-
-// const toggleLikeMuro = async (req, res, next) => {
-//   try {
-//     const { wallId } = req.params;
-//     // vamos a tener el middleware de auth por lo cual se crea req.user
-//     const { _id } = req.user;
-
-//     if (req.user.murosLikes.includes(wallId)) {
-//       try {
-//         await User.findByIdAndUpdate(_id, {
-//           $pull: { murosLikes: wallId },
-//         });
-
-//         try {
-//           await Wall.findByIdAndUpdate(wallId, {
-//             $pull: { likes: _id },
-//           });
-
-//           return res.status(200).json({
-//             action: "disliked",
-//             user: await User.findById(_id).populate("murosLikes"),
-//             wall: await Wall.findById(wallId).populate("likes"),
-//           });
-//         } catch (error) {
-//           return res.status(404).json({
-//             error: "no update wall - likes",
-//             message: error.message,
-//           });
-//         }
-//       } catch (error) {
-//         return res.status(404).json({
-//           error: "no update user-  murosLikes",
-//           message: error.message,
-//         });
-//       }
-//     } else {
-//       try {
-//         await User.findByIdAndUpdate(_id, {
-//           $push: { murosLikes: wallId },
-//         });
-
-//         try {
-//           await Wall.findByIdAndUpdate(wallId, {
-//             $push: { likes: _id },
-//           });
-
-//           return res.status(200).json({
-//             action: "like",
-//             user: await User.findById(_id).populate("murosLikes"),
-//             wall: await Wall.findById(wallId).populate("likes"),
-//           });
-//         } catch (error) {
-//           return res.status(404).json({
-//             error: "no update wall - likes",
-//             message: error.message,
-//           });
-//         }
-//       } catch (error) {
-//         return res.status(404).json({
-//           error: "no update user-  murosLikes",
-//           message: error.message,
-//         });
-//       }
-//     }
-//   } catch (error) {
-//     return res.status(404).json(error.message);
-//   }
-// };
-
-module.exports = { deleteMessagesByUser,createMessage, getById };
+module.exports = { 
+  deleteMessagesByUser,
+  createMessage, 
+  getById, 
+  toggleLikeMuro 
+};
--- Backend/src/api/routes/Message.routes.js
+++ Backend/src/api/routes/Message.routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const MessageRoutes = express.Router();
 
-const{createMessage, deleteMessagesByUser, getById}= require("../controllers/Message.controllers")
+const{
+    createMessage, 
+    deleteMessagesByUser, 
+    getById,
+    toggleLikeMuro
+}= require("../controllers/Message.controllers");
 const { isAuth } = require("../../middleware/auth.middleware");
 
 
 MessageRoutes.post("/:idRecipient", [isAuth], createMessage);
 MessageRoutes.delete('/:chatId/:userId', deleteMessagesByUser);
 MessageRoutes.get("/findById/:id", getById);
+MessageRoutes.patch("/like/:wallId", [isAuth], toggleLikeMuro);
+
+
 module.exports = MessageRoutes;
