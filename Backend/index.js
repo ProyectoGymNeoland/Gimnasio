68b763a695bd9ef2e54fd6f30012a78da5982436
RubenHH019 <rhernandezh019@gmail.com>
2024-04-28 04:08:39 +0200

    Controladores ruben y marta,el server conecta

--- Backend/index.js
+++ Backend/index.js
@@ -33,11 +33,11 @@ app.use(express.urlencoded({ limit: "5mb", extended: false }));
 const ActivitiesRoutes = require("./src/api/routes/Activities.routes");
 app.use("/api/v1/activities/", ActivitiesRoutes);
 
-// const ActivityToDayRoutes = require("./src/api/routes/ActivityToDay.routes");
-// app.use("/api/v1/activityToDay/", ActivityToDayRoutes);
+const ActivityToDayRoutes = require("./src/api/routes/ActivityToDay.routes");
+app.use("/api/v1/activityToDay/", ActivityToDayRoutes);
 
-// const DayRoutes = require("./src/api/routes/Day.routes");
-// app.use("/api/v1/day/", DayRoutes);
+const DayRoutes = require("./src/api/routes/Day.routes");
+app.use("/api/v1/day/", DayRoutes);
 
 const MessageRoutes = require("./src/api/routes/Message.routes");
 app.use("/api/v1/message/", MessageRoutes);
