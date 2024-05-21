39bb4bba72f732ccfe822cd4851529e7d2f89b68
Evelyn <evelyndb@gmail.com>
2024-05-21 22:46:56 +0200

    Cambio ruta

--- Frontend/src/pages/Main.jsx
+++ Frontend/src/pages/Main.jsx
@@ -5,28 +5,38 @@ import { About } from "../components";
 
 export const Main = () =>{
     return (
-    <>
+      <>
         <div className="nav-container">
-        <nav className="navbar">
+          <nav className="navbar">
             <ul>
-                <li><a href="#about">About</a></li>
-                <li><Link to="/muro">Muro</Link></li>
-                <li><Link to="/contacto">Contacto</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/actividades">Actividades</Link></li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <Link to="/muro">Muro</Link>
+              </li>
+              <li>
+                <Link to="/contacto">Contacto</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/activities/feed">Actividades</Link>
+              </li>
             </ul>
-        </nav>
+          </nav>
         </div>
         <div className="main-container">
-            <main className="main-content">
-                <About />
-            </main>
+          <main className="main-content">
+            <About />
+          </main>
         </div>
         <div className="footer-container">
-            <footer className="footer">
-                <p>&copy; 2024 My Website. All rights reserved.</p>
-            </footer>
+          <footer className="footer">
+            <p>&copy; 2024 My Website. All rights reserved.</p>
+          </footer>
         </div>
-    </>
-    )
+      </>
+    );
 }
