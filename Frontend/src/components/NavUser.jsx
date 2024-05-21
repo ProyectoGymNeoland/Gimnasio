45ed2ab0879a30590a075d13bd52b6dce7629551
sarodri <sandra.ropero1987@gmail.com>
2024-05-21 09:45:14 +0200

    NavUser

--- Frontend/src/components/NavUser.jsx
+++ Frontend/src/components/NavUser.jsx
@@ -1,69 +1,46 @@
 import { Link } from "react-router-dom";
 import "./NavUser.css";
-import { useAuth } from "../context/authContext";
+import "../pages/Register"
+
 
 export const NavUser = () => {
   
   return (
-    <div className="containerNavUser">
-        <div className="navUser">
-        <Link to="/profile/mychats">
-        <button className="button-navUser" id="nav-chat"> Chats
-        <img
-          src="https://res.cloudinary.com/dq186ej4c/image/upload/v1686125399/pngwing.com_npd5sa.png"
-          alt="go to Chat"
-          className="iconNav"
-        />
-        </button>
-        
-      </Link>
+    <div className="form-wrap">
+        <div className="form-group user-nav">
+            <Link to="/profile/mychats">
+                <button className="btn" id="nav-chat"> Chats
+                </button>
+            </Link>
 
-      <Link to="/profile/reviews">
-      <button className="button-navUser" id="nav-review"> Reviews
-       <img
-          src="https://res.cloudinary.com/dq186ej4c/image/upload/v1686125391/Change_User_icon-icons.com_55946_lypx2c.png"
-          alt="go to review"
-          className="iconNav"
-        />
-        </button>
-       
-      </Link>
+            <Link to="/profile/reviews">
+                <button className="btn" id="nav-review"> Reviews
+                </button>
+            </Link>
 
-      <Link to="/profile/books">
-      <button className="button-navUser" id="nav-books">Books
-         <img
-          src="https://res.cloudinary.com/dq186ej4c/image/upload/v1686125391/Change_User_icon-icons.com_55946_lypx2c.png"
-          alt="go to books"
-          className="iconNav"
-        />
-      </button>
-       
-      </Link>
+            <Link to="/profile/reviews">
+                <button className="btn" id="nav-likesWall"> Likes
+                </button>
+            </Link>
 
-      <Link to="/profile/activitiesfav"> 
-      <button className="button-navUser" id="nav-activitiesfav"> Activies
-           <img
-          src="https://res.cloudinary.com/dq186ej4c/image/upload/v1686125391/Change_User_icon-icons.com_55946_lypx2c.png"
-          alt="go to activities favorites"
-          className="iconNav"
-        />
-      </button>
-     
-      </Link>
-      </div>
-      <div className="userInfo">
+            <Link to="/profile/books">
+                <button className="btn" id="nav-books">Books
+                </button>
+            </Link>
+
+            <Link to="/profile/activitiesfav"> 
+                <button className="btn" id="nav-activitiesfav"> Activies
+                </button>
+            </Link>
+
+            <Link to="/profile/activitiesfav"> 
+                <button className="btn" id="nav-instructorsfav">Instructors
+                </button>
+            </Link>
+        </div>
+        <div className="form-group user-info">
                 AQUI SE PINTA LO QUE ENLACE CADA BOTON
-      </div>
-
-      
-
-      {/* <img
-        src="https://res.cloudinary.com/dq186ej4c/image/upload/v1686140226/eliminar_user_rmwoeg.png"
-        alt="user delete button"
-        className="iconNav iconDeleteUser"
-        onClick={() => useDeleteUser(setUser, setDeleteUser)}
-        // customhook que hace la peticion al servicio de delete User y setea el usuario a null en el contexto
-      /> */}
+        </div>
     </div>
   );
 };
