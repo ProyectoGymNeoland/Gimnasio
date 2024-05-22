5524e95c0e5f9c1e66877e42abfbea38f64bd621
mmoralesgr93 <mmoralesgr93@gmail.com>
2024-05-22 08:05:25 +0200

    day

--- Frontend/src/pages/Main.jsx
+++ Frontend/src/pages/Main.jsx
@@ -5,6 +5,9 @@ export const Main = () =>{
     return (
     <>
         <div className="nav-container">
+            <div className="logogym">
+                <img src="https://res.cloudinary.com/dpw6wsken/image/upload/v1716316187/logofinal_csiazn.png"></img>
+            </div>
         <nav className="navbar">
             <ul>
                 <li><Link to="/about">About</Link></li>
@@ -14,6 +17,7 @@ export const Main = () =>{
                 <li><Link to="/actividades">Actividades</Link></li>
             </ul>
         </nav>
+            //*navuser*//
         </div>
         <div>
             <main className="main-content">
