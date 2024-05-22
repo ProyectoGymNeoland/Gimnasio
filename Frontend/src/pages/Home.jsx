73181c62140e0a9d07c5f4ec298b2b987c6ec495
Evelyn <evelyndb@gmail.com>
2024-05-22 09:52:19 +0200

    Activities Home Card Component

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
 import './Home.css'; 
 import "./Nav.css"
-import { About } from "../components";
+import { About, ActivitiesHomeCard } from "../components";
 
 export const Home = () =>{
     return (
       <>
-       
+     
             <About />
+            <ActivitiesHomeCard/>
+           
        
       </>
     );
