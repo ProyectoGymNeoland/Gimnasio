545379e2cfb4fa5b22d1d15f6c38ec39ebba7545
Evelyn <evelyndb@gmail.com>
2024-05-25 12:00:37 +0200

    css Activity Detail

--- Frontend/src/components/ActivityDetail.jsx
+++ Frontend/src/components/ActivityDetail.jsx
@@ -1,21 +1,26 @@
-// src/components/ActivityDetail.jsx
+import './ActivityDetail.css';
 import React, { useEffect, useState } from 'react';
 import { getById } from '../services/activities.service';
 
 const ActivityDetail = ({ activity }) => {
-    if (!activity) {
-      return <div>Crear página 404 VER CON PEDRO...</div>;
-    }
-    
+  if (!activity) {
+    return <div>Crear página 404 VER CON PEDRO...</div>;
+  }
+
   return (
     <div className="activity-detail">
       <h1>{activity.name}</h1>
-      <p>Número de plazas: {activity.spots}</p>
-      <p>Tipo de actividad: {activity.type}</p>
-      <p>Status: {activity.status ? 'Disponible' : 'No disponible'}</p>
+      <p className="spots">Número de plazas: {activity.spots}</p>
+      <p className="type">Tipo de actividad: {activity.type}</p>
+      <p className="type">
+        Status:
+        <span className={activity.status ? 'available' : 'not-available'}>
+          {activity.status ? 'Disponible' : 'No disponible'}
+        </span>
+      </p>
       <img src={activity.image} alt={activity.name} />
       <h3>Te contamos en qué consiste</h3>
-      <p>{activity.description}</p>
+      <p className="description">{activity.description}</p>
     </div>
   );
 };
