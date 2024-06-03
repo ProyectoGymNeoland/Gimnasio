9fcaf03c8efe6f1251d2636bc4410cd6d71938de
Your Name <you@example.com>
2024-06-03 10:31:15 +0200

    cambios muro boton y status

--- Frontend/src/components/WallDetail.jsx
+++ Frontend/src/components/WallDetail.jsx
@@ -26,16 +26,11 @@ const handleSendMessage = async () => {
       <p className="spots">Número de plazas: {wall.spots}</p>
       <p className="type">Tipo de actividad: {wall.type}</p>
       <p className="type">
-        Status:
-        <span className={wall.status ? 'available' : 'not-available'}>
-          {wall.status ? 'Disponible' : 'No disponible'}
-        </span>
       </p>
       <img src={wall.image} alt={wall.name} />
       <h3>Te contamos en qué consiste</h3>
       <p className="description">{wall.content}</p>
       <button onClick={ handleButtonClick }>Comentarios</button>
-      <button onClick={ handleButtonClick }>Reservar</button>
     <div className="chat-input">
               <input 
                   type="text" 
